b3d9a52a0a08a8b7c7524cfc33ae8733c558ba0d
NoeCutz <noecutz03@gmail.com>
2017-11-26 20:58:48 -0600

    Se creo el UI y la funcionalidad inicial del mapeo del salon

--- app/src/main/java/com/fmat/uady/cec/computerCenterDetail/ComputerCenterDetailActivity.java
+++ app/src/main/java/com/fmat/uady/cec/computerCenterDetail/ComputerCenterDetailActivity.java
@@ -3,14 +3,21 @@ package com.fmat.uady.cec.computerCenterDetail;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.fmat.uady.cec.R;
 import com.fmat.uady.cec.listComputerCenter.ComputerCenterAdapter;
+import com.fmat.uady.cec.model.Computer;
+import com.fmat.uady.cec.model.ComputerData;
+
+import java.util.ArrayList;
 
 public class ComputerCenterDetailActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private ComputerCenterDetailAdapter adapter;
     private String nameCenter ;
 
 
@@ -29,6 +36,20 @@ public class ComputerCenterDetailActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = (RecyclerView) findViewById(R.id.list_computer);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 5);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ComputerData datos = new ComputerData();
+
+        ArrayList<Computer> computers = datos.getComputersByCenter(nameCenter);
+
+        adapter = new ComputerCenterDetailAdapter(computers, getApplicationContext());
+
+        recyclerView.setAdapter(adapter);
+
 
     }
 
